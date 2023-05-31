c0fcccda2cc0142a50e06b171c922a5801839489
weiji-Feng <kehuafeng2002@gmail.com>
2023-05-31 15:43:05 +0800

    Site updated: 2023-05-31 15:43:04

--- js/foot.js
+++ js/foot.js
@@ -1,6 +1,6 @@
 // 动态心跳，更改自己的名称
 $(document).ready(function(e){
-    $('.copyright').html('2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By weiji-Feng');
+    $('.copyright').html('2023 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By weiji-Feng');
 })
 
 $(document).ready(function(e){
@@ -9,7 +9,7 @@ $(document).ready(function(e){
 
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('<font style=color:#afb4db>' + 'weiji-Feng的后花园已经安全运行<span id="span_dt_dt" style="color: #000;"></span>' + '</font>');
+$('.framework-info').html('<font style=color:#000>' + 'weiji-Feng的后花园已经安全运行<span id="span_dt_dt" style="color: #000;"></span>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("23/5/2023 0:0:0");
 today=new Date();
