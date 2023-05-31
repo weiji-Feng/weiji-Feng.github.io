1d3401751a4ee7bb5e3e4affea7299e6a3566064
weiji-Feng <kehuafeng2002@gmail.com>
2023-05-31 15:46:06 +0800

    Site updated: 2023-05-31 15:46:05

--- js/foot.js
+++ js/foot.js
@@ -9,7 +9,7 @@ $(document).ready(function(e){
 
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('<font style=color:#000> weiji-Feng的后花园已经安全运行<span id="span_dt_dt" style="color: #000;"></span>');
+$('.framework-info').html('暗格已经安全运行<span id="span_dt_dt" style="color: #000;"></span>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("23/5/2023 0:0:0");
 today=new Date();
